5ed7c592e721c9e7c6f0d8c092545ffacf89b963
pphila <efl2ice89@gmail.com>
2023-03-26 11:10:54 -0700

    add third test and passed

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -2,12 +2,26 @@ import ticketListReducer from "../../reducers/ticket-list-reducer";
 
 describe('ticketListReducer', () => {
   
+  const currentState = {
+    1: {
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux action is not working correctly',
+      id: 1
+    }, 2: {
+      names: 'Jasmine and Justine',
+      location: '2a',
+      issue: 'Reducer has side effects.',
+      id: 2
+    }
+  }
+  
   let action;
   const ticketData = {
-    names: 'Ryan & Aimen',
-    location: '4b',
-    issue: 'Redux actio is now working correctly',
-    id: 1
+      names: 'Ryan & Aimen',
+      location: '4b',
+      issue: 'Redux actio is now working correctly',
+      id: 1
   };
   
   test('Should return default state if there is no action type passed into reducer', () => {
@@ -33,5 +47,20 @@ describe('ticketListReducer', () => {
       }
     });
   });
-  
+
+  test('Should successfully delete a ticket', () => {
+    action = {
+      type: 'DELETE_TICKET',
+      id: 1
+    };
+    expect(ticketListReducer(currentState, action)).toEqual({
+      2: {
+        names: 'Jasmine and Justine',
+        location: '2a',
+        issue: 'Reducer has side effects.',
+        id: 2
+      }
+    });
+  });
+
 });
